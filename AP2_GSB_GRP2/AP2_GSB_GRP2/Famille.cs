5e45fabd24920a79f80ce16e0d15d4ba26b918d1
ilies amine <iliesamineizikki@gmail.com>
2023-11-17 17:31:18 +0100

    <feat>(création classe):création de la classe Famille

---
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/Famille.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP2_GSB_GRP2
+{
+    internal class Famille
+    {
+
+        public Famille(string code)
+        {
+            
+        }
+
+    }
+}
---
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/Medicament.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP2_GSB_GRP2
+{
+    internal class Medicament
+    {
+    }
+}
