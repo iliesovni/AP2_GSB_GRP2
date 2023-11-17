f61481a00bca3837d58cc39c1489eeb1e99f943a
Dhelil Medjeni <medjenidhelil@gmail.com>
2023-11-17 17:25:51 +0100

    <feat>(Ajout des Mutateurs/Accesseurs

--- AP2_GSB_GRP2/AP2_GSB_GRP2/EtapeNormee.cs
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/EtapeNormee.cs
@@ -10,13 +10,24 @@ namespace AP2_GSB_GRP2
     {
         // attributs de la classe
         private int etp_norme;
-        private string etp_date_norme_date;
+        private string etp_date_norme;
 
         // constructeur
         public Etape(int laNorme, string ladateNorme)
         {
             this.etp_norme = laNorme;
-            this.etp_date_norme_date = ladateNorme;
+            this.etp_date_norme = ladateNorme;
+        }
+
+        // Mutateurs, Accesseurs
+        public int getLaNorme()
+        {
+            return this.etp_norme;
+        }
+
+        public string getLaDateNorme()
+        {
+            return this.etp_date_norme;
         }
     }
 }
