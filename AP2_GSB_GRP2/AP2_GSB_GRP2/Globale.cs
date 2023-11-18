3d3fd965ee072365db1cddc27d436641d554a227
Dhelil Medjeni <medjenidhelil@gmail.com>
2023-11-19 00:59:16 +0100

    <feat>(Ajout dicos Famille/Medoc et Liste Décision)

--- AP2_GSB_GRP2/AP2_GSB_GRP2/Globale.cs
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/Globale.cs
@@ -8,6 +8,10 @@ namespace AP2_GSB_GRP2
 {
     static class Globale
     {
+        public static Dictionary<string, Famille> lesFamilles; // clé du dico : code famille
+        public static Dictionary<string, Medicament> lesMedicaments; // clé du dico : dépôt légal
         public static List<Etape> lesEtapes;
+        public static List<Decision> lesDecisions;
+
     }
 }
