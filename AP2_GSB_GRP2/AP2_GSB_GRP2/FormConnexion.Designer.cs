d82a046b5c0dde0d2a1385227ffd01a4de2fbdb2
Dhelil Medjeni <medjenidhelil@gmail.com>
2023-12-01 16:49:20 +0100

    <feat>(Création de la Form et ajout du style)

---
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/FormConnexion.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace AP2_GSB_GRP2
+{
+    partial class FormConnexion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Connexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Name = "Connexion";
+            this.Text = "Connexion";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
