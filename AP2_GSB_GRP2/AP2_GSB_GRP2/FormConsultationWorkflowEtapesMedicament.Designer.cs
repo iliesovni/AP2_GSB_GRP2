96f9898d5e70287bdb02a1a549b19609d2278874
Dhelil Medjeni <medjenidhelil@gmail.com>
2023-12-11 16:02:20 +0100

    <feat>(Front du form terminé)

---
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/FormConsultationWorkflowEtapesMedicament.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace AP2_GSB_GRP2
+{
+    partial class FormConsultationWorkflowEtapesMedicament
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lvMedoc = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.lvEtapes = new System.Windows.Forms.ListView();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Image = global::AP2_GSB_GRP2.Properties.Resources.LOGO_GSB_50par50;
+            this.pictureBoxLogo.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(159, 109);
+            this.pictureBoxLogo.TabIndex = 0;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 31.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(183, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1050, 70);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Consultation des étapes d\'un médicament";
+            // 
+            // lvMedoc
+            // 
+            this.lvMedoc.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvMedoc.Location = new System.Drawing.Point(163, 160);
+            this.lvMedoc.Name = "lvMedoc";
+            this.lvMedoc.Size = new System.Drawing.Size(357, 404);
+            this.lvMedoc.TabIndex = 2;
+            this.lvMedoc.UseCompatibleStateImageBehavior = false;
+            this.lvMedoc.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Dépot légal";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nom commercial";
+            this.columnHeader2.Width = 130;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Code famille";
+            this.columnHeader3.Width = 100;
+            // 
+            // lvEtapes
+            // 
+            this.lvEtapes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lvEtapes.Location = new System.Drawing.Point(673, 160);
+            this.lvEtapes.Name = "lvEtapes";
+            this.lvEtapes.Size = new System.Drawing.Size(357, 404);
+            this.lvEtapes.TabIndex = 3;
+            this.lvEtapes.UseCompatibleStateImageBehavior = false;
+            this.lvEtapes.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Numéro étape";
+            this.columnHeader4.Width = 120;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Libelle étape";
+            this.columnHeader5.Width = 130;
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Location = new System.Drawing.Point(936, 611);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(94, 29);
+            this.btnQuitter.TabIndex = 4;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.lvEtapes);
+            this.Controls.Add(this.lvMedoc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Name = "Form1";
+            this.Text = "FormConsultationWorkflowEtapesMedicaments";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBoxLogo;
+        private Label label1;
+        private ListView lvMedoc;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ListView lvEtapes;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private Button btnQuitter;
+    }
+}
