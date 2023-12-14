4b2ffffda6b77298b37b4d34a01b9a3eea86753a
ilies amine <iliesamineizikki@gmail.com>
2023-12-14 11:47:09 +0100

    <feat>(Affichage des données):fonctionne, reste la verif d'autorisation

--- AP2_GSB_GRP2/AP2_GSB_GRP2/NbMedicamentsAutorisesParFamille.cs
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/NbMedicamentsAutorisesParFamille.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace AP2_GSB_GRP2
 {
@@ -20,11 +21,29 @@ namespace AP2_GSB_GRP2
 
         private void LV_Famille_Autorises_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            if (LV_Famille_Autorises.SelectedItems.Count > 0)
+            {
+                LV_Medicaments_Autorises.Items.Clear();
+                string valeurPremiereColonne = LV_Famille_Autorises.SelectedItems[0].SubItems[0].Text;
+                string connstring = "Data Source = BTS2022-19\\SQLEXPRESS;Initial Catalog=GSB_gesAMM;Integrated Security=true;User Id=DOMADJ\\izikkii";
+                SqlConnection con = new SqlConnection(connstring);
+                con.Open();
+                string query = "Select * from MEDICAMENT Where FAM_CODE_MEDICAMENT = '" + valeurPremiereColonne + "';";
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ListViewItem lvi = new ListViewItem(reader.GetValue(0).ToString());
+                    lvi.SubItems.Add(reader.GetValue(1).ToString());
+                    lvi.SubItems.Add(reader.GetValue(6).ToString());
+                    LV_Medicaments_Autorises.Items.Add(lvi);
+                }
+            }
         }
 
         private void NbMedicamentsAutorisesParFamille_Load(object sender, EventArgs e)
         {
+            LV_Famille_Autorises.Items.Clear();
             string connstring = "Data Source = BTS2022-19\\SQLEXPRESS;Initial Catalog=GSB_gesAMM;Integrated Security=true;User Id=DOMADJ\\izikkii";
             SqlConnection con = new SqlConnection(connstring);
             con.Open();
@@ -35,6 +54,7 @@ namespace AP2_GSB_GRP2
             {
                 ListViewItem lvi = new ListViewItem(reader.GetValue(0).ToString());
                 lvi.SubItems.Add(reader.GetValue(1).ToString());
+                lvi.SubItems.Add(reader.GetValue(2).ToString());
                 LV_Famille_Autorises.Items.Add(lvi);
             }
         }
