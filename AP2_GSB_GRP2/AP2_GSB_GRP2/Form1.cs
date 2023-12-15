9e6b98d4fd8447fbc040d2703db885c9fbf18240
ilies amine <iliesamineizikki@gmail.com>
2023-12-15 13:53:31 +0100

    <feat>(données):ajout des données dans le dictionnaire de classe globale

--- AP2_GSB_GRP2/AP2_GSB_GRP2/Form1.cs
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/Form1.cs
@@ -1,3 +1,5 @@
+using System.Data.SqlClient;
+
 namespace AP2_GSB_GRP2
 {
     public partial class Form1 : Form
@@ -10,9 +12,20 @@ namespace AP2_GSB_GRP2
         private void Form1_Load(object sender, EventArgs e)
         {
             NbMedicamentsAutorisesParFamille form2 = new NbMedicamentsAutorisesParFamille();
-
-            // Affichez le Form2
             form2.Show();
+
+            string connstring = "Data Source = BTS2022-19\\SQLEXPRESS;Initial Catalog=GSB_gesAMM;Integrated Security=true;User Id=DOMADJ\\izikkii";
+            SqlConnection con = new SqlConnection(connstring);
+            con.Open();
+            string query = "Select * from FAMILLE;";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Famille laFamille = new Famille(reader.GetValue(0).ToString(), reader.GetValue(1).ToString());
+                Globale.lesFamilles.Add(reader.GetValue(1).ToString(), laFamille);
+            }
+            con.Close();
         }
     }
 }
