8e0651b3e5896454c1c68b6e38c1981cbbb75e00
Dhelil Medjeni <medjenidhelil@gmail.com>
2023-12-15 11:59:06 +0100

    <feat>(Commencement Back AjoutMedoc)

--- AP2_GSB_GRP2/AP2_GSB_GRP2/FormAjouterMedicament.cs
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/FormAjouterMedicament.cs
@@ -7,5 +7,14 @@ namespace AP2_GSB_GRP2
             InitializeComponent();
         }
 
+        private void FormAjouterMedicament_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnnuler_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
