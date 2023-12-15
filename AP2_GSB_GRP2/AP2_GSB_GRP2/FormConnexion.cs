526e29d9df2039d2ba4a9b73741faf8e91f936b4
Dhelil Medjeni <medjenidhelil@gmail.com>
2023-12-15 14:10:39 +0100

    <feat>(Page Connexion: Ajout de commentaires)

--- AP2_GSB_GRP2/AP2_GSB_GRP2/FormConnexion.cs
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/FormConnexion.cs
@@ -25,17 +25,30 @@ namespace AP2_GSB_GRP2
 
         private void btnConnecter_Click(object sender, EventArgs e)
         {
+            // Chaîne de connexion à la BDD
             string connexion = "Data Source = BTS2022-24\\SQLEXPRESS01; Initial Catalog = AP2-GP2; Integrated Security=true";
+
+            // Initialisation d'une connexion à la BDD à partir de la chaîne de connexion
             SqlConnection con = new SqlConnection(connexion);
+
+            // Ouverture de la connexion à la BDD
             con.Open();
+
+            // Création d'un requête
             string requete = "SELECT * FROM UTILISATEUR";
+
+            // Création d'un objet "Command" prenant la requete et la connexion en paramètre
             SqlCommand commande = new SqlCommand(requete, con);
+
+            // Exécution de la requête
             SqlDataReader reader = commande.ExecuteReader();
 
             bool utilisateurTrouve = false;
 
+            // Parcours des données récupérées
             while (reader.Read())
             {
+                // On vérifie si le nom et le mdp correspondent à ceux en BDD
                 if (tbUsername.Text == reader.GetValue(1).ToString() && tbPassword.Text == reader.GetValue(7).ToString())
                 {
                     utilisateurTrouve = true;
@@ -52,7 +65,8 @@ namespace AP2_GSB_GRP2
                 MessageBox.Show("Erreur lors de la saisie de vos identifiants");
             }
 
-            con.Close(); // Fermer la connexion après utilisation
+            // Fermeture de la connexion à la base de données
+            con.Close();
 
 
             // On affiche le menu et on ferme cette form
