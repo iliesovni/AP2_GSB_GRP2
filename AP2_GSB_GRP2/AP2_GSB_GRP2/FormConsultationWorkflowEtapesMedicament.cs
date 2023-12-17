2c533d2ff06d5e1a46fe40e74a8e96477d1b4e5e
Dhelil Medjeni <medjenidhelil@gmail.com>
2023-12-17 22:08:44 +0100

    <feat>(Ajustements)

--- AP2_GSB_GRP2/AP2_GSB_GRP2/FormConsultationWorkflowEtapesMedicament.cs
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/FormConsultationWorkflowEtapesMedicament.cs
@@ -63,45 +63,9 @@ namespace AP2_GSB_GRP2
 
                 lvMedoc.Items.Add(uneLiggne);
             }
-
-
         }
 
-        public void getLesEtapes()
-        {
-            Globale.lesEtapes = new List<Etape>();
-
-            SqlCommand maRequete = new SqlCommand("prc_getEtapes");
-            maRequete.CommandType = System.Data.CommandType.StoredProcedure;
-            maRequete.ExecuteNonQuery();
-
-            SqlDataReader SqlDataRead = maRequete.ExecuteReader();
-
-            while (SqlDataRead.Read())
-            {
-                int num = Convert.ToInt32(SqlDataRead["ETP_NUM"]);
-                string libelle = SqlDataRead["ETP_LIBELLE"].ToString();
-                string norme = SqlDataRead["ETP_NORME"].ToString();
-                DateTime dateNorme = DateTime.MaxValue;
-
-                if (SqlDataRead["ETP_DATE_NORME"].ToString() != "")
-                {
-                    dateNorme = Convert.ToDateTime(SqlDataRead["ETP_DATE_NORME"]);
-                }
-
-                if (norme == "" && dateNorme == DateTime.MaxValue)
-                {
-                    Globale.lesEtapes.Add(new Etape(num, libelle));
-                }
-                else
-                {
-                    Globale.lesEtapes.Add(new EtapeNormee(norme, dateNorme, num, libelle));
-                }
-            }
-
-            SqlDataRead.Close();
-        }
-
+       
         private void lvMedoc_SelectedIndexChanged(object sender, EventArgs e)
         {
             // Chaine de connexion à la BDD
