35d7e0469d35f013502fd933722b5a496ca35a94
Dhelil Medjeni <medjenidhelil@gmail.com>
2023-12-17 16:49:21 +0100

    <feat>(Classe Decision créer)

---
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/Decision.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP2_GSB_GRP2
+{
+    internal class Decision
+    {
+        private int id;
+        private string libelle;
+
+        public Decision(int leId, string leLibelle)
+        {
+            this.id = leId;
+            this.libelle = leLibelle;
+        }
+
+        public int getLeId()
+        {
+            return this.id;
+        }
+
+        public string getLeLibelle()
+        {
+            return this.libelle;
+        }
+
+        public void setLeId(int leId)
+        {
+            this.id = leId;
+        }
+
+        public void setLeLibelle(string leLibelle)
+        {
+            this.libelle = leLibelle;
+        }
+    }
+}
