0580663a56d548253dd8ad9fe9cfae845e6916ce
Dhelil Medjeni <medjenidhelil@gmail.com>
2023-12-17 16:47:47 +0100

    <feat>(Classe Worflow créer)

--- AP2_GSB_GRP2/AP2_GSB_GRP2/FormConsultationWorkflowEtapesMedicament.cs
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/FormConsultationWorkflowEtapesMedicament.cs
@@ -1,3 +1,5 @@
+using System.Data.SqlClient;
+
 namespace AP2_GSB_GRP2
 {
     public partial class FormConsultationWorkflowEtapesMedicament : Form
@@ -14,7 +16,71 @@ namespace AP2_GSB_GRP2
 
         private void FormConsultationWorkflowEtapesMedicament_Load(object sender, EventArgs e)
         {
+            Globale.lesMedicaments.Clear();
 
+            // Chaîne de connexion à la BDD
+            string connexion = "Data Source = BTS2022-24\\SQLEXPRESS01; Initial Catalog = AP2-GP2; Integrated Security=true; User Id=DOMADJ\\medjenid";
+            // Initialisation d'une connexion à la BDD à partir de la chaîne de connexion
+            SqlConnection con = new SqlConnection(connexion);
+            // Ouverture de la connexion à la BDD
+            con.Open();
+
+            Globale.lesMedicaments = new Dictionary<string, Medicament>();
+
+            SqlCommand requete = new SqlCommand("prc_getMedicaments", con);
+            requete.CommandType = System.Data.CommandType.StoredProcedure;
+            requete.ExecuteNonQuery();
+
+            SqlDataReader SqlDataRead = requete.ExecuteReader();
+
+            while(SqlDataRead.Read())
+            {
+                string depotLegal = SqlDataRead["MED_DEPOTLEGAL"].ToString();
+                string nomCommercial = SqlDataRead["MED_NOMCOMMERCIAL"].ToString();
+                string composition = SqlDataRead["MED_COMPOSITION"].ToString();
+                string effets = SqlDataRead["MED_EFFETS"].ToString();
+                string contreIncic = SqlDataRead["MED_CONTREINDIC"].ToString();
+                string medAmm = SqlDataRead["MED_AMM"].ToString();
+                string famCode = SqlDataRead["FAM_CODE_MEDCIAMENT"].ToString();
+
+                Globale.lesMedicaments.Add(depotLegal, new Medicament(depotLegal, nomCommercial, composition, effets, contreIncic, medAmm));
+            }
+
+            SqlDataRead.Close();
+
+            lvMedoc.Items.Clear();
+
+            Dictionary<string, Medicament>.KeyCollection lesClesMedicaments = Globale.lesMedicaments.Keys;
+
+            foreach (string lacle in lesClesMedicaments)
+            {
+                Medicament unMed = Globale.lesMedicaments[lacle];
+
+                ListViewItem uneLiggne = new ListViewItem();
+                uneLiggne.Text = unMed.getDepotLegal();
+                uneLiggne.SubItems.Add(unMed.getNomCommercial());
+                uneLiggne.SubItems.Add(unMed.getAmm());
+
+                lvMedoc.Items.Add(uneLiggne);
+            }
+
+
+        }
+
+        private void lvMedoc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string leClick = lvMedoc.Items[lvMedoc.FocusedItem.Index].Text;
+
+            lvEtapes.Items.Clear();
+            foreach (Workflow E in Globale.lesMedicaments[numLigClic].getLesEtapes())
+            {
+                ListViewItem ligne = new ListViewItem();
+                ligne.Text = E.getEtape().getNum().ToString();
+                ligne.SubItems.Add(numLigClic);
+                ligne.SubItems.Add(E.getDateDecision().ToString());
+
+                lv_med_workflow.Items.Add(ligne);
+            }
         }
     }
 }
