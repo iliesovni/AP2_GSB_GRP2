6df231e72c554b9fe8f08b8e47114b095b4366cb
ilies amine <iliesamineizikki@gmail.com>
2023-12-18 13:37:03 +0100

    <feat>(affichage):affichage des étapes relatives au médicament choisi

--- AP2_GSB_GRP2/AP2_GSB_GRP2/MedicamentsEnCoursDeValidation.cs
+++ AP2_GSB_GRP2/AP2_GSB_GRP2/MedicamentsEnCoursDeValidation.cs
@@ -34,10 +34,34 @@ namespace AP2_GSB_GRP2
             SqlDataReader reader = cmd.ExecuteReader();
             while (reader.Read())
             {
-                    ListViewItem lvi = new ListViewItem(reader.GetValue(0).ToString());
-                    lvi.SubItems.Add(reader.GetValue(1).ToString());
-                    lvi.SubItems.Add(reader.GetValue(6).ToString());
-                    LV_Medicaments_Validation.Items.Add(lvi);
+                ListViewItem lvi = new ListViewItem(reader.GetValue(0).ToString());
+                lvi.SubItems.Add(reader.GetValue(1).ToString());
+                lvi.SubItems.Add(reader.GetValue(6).ToString());
+                LV_Medicaments_Validation.Items.Add(lvi);
+            }
+        }
+
+        private void LV_Medicaments_Validation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LV_Medicaments_Validation_Click(object sender, EventArgs e)
+        {
+            LV_Etape_Validation.Items.Clear();
+            string valeurPremiereColonne = LV_Medicaments_Validation.SelectedItems[0].SubItems[0].Text;
+            string connstring = "Data Source = DESKTOP-41R7HMR\\SQLEXPRESS;Initial Catalog=GSB_gesAMM;Integrated Security=true;User Id=DESKTOP-41R7HMR\\iliesjaaj;MultipleActiveResultSets=True";
+            SqlConnection con = new SqlConnection(connstring);
+            con.Open();
+            string query = "Select * from ETAPE INNER JOIN DERNIERE_VALIDATION ON ETAPE.ETP_NUM = DERNIERE_VALIDATION.ETP_NUM_DV INNER JOIN ETAPE_NORME ON DERNIERE_VALIDATION.ETP_NUM_DV = ETAPE_NORME.ETP_NUM_NORME WHERE MED_DEPOTLEGAL_DV = '" + valeurPremiereColonne + "';";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                ListViewItem lvi = new ListViewItem(reader.GetValue(5).ToString());
+                lvi.SubItems.Add(reader.GetValue(0).ToString());
+                lvi.SubItems.Add(reader.GetValue(2).ToString());
+                LV_Etape_Validation.Items.Add(lvi);
             }
         }
     }
